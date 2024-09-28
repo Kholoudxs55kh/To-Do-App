1bcd60a3aff84905ba1c442b65dad02b3462d88b
kholoudxs55kh <kfattem@gmail.com>
2024-09-28 07:41:51 +0300

    feat[backend]: Add entery point for the project

---
+++ db/index.ts
@@ -0,0 +1,35 @@
+
+import cors from 'cors'
+import morgan from 'morgan'
+import createError from 'http-errors'
+import dotenv from 'dotenv'
+import express, { NextFunction, Request, Response } from 'express'
+import taskRoutes from './routes/task.routes'
+import { DeleteTaskIfPassesWeekDeleted } from './middlewares/CheckTaskDeletion'
+
+
+export const app = express()
+
+DeleteTaskIfPassesWeekDeleted()
+
+app.use(cors())
+
+
+dotenv.config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(morgan('dev'))
+
+
+app.use('/api', taskRoutes)
+
+
+
+app.use((req:Request, res:Response, next: NextFunction) => {
+    next(createError.NotFound())
+})
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`live at http://localhost:${PORT}`))
