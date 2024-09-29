59d4acc0adc2c2758d2fbd4a0a45e50bc51f3aef
kholoudxs55kh <kfattem@gmail.com>
2024-09-29 17:15:17 +0300

    fix[backend]: Fix `DTO`, `ZOD` after adding description and labels

--- db/index.ts
+++ db/index.ts
@@ -1,35 +1,29 @@
+import cors from "cors";
+import morgan from "morgan";
+import createError from "http-errors";
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import taskRoutes from "./routes/task.routes";
+import { DeleteTaskIfPassesWeekDeleted } from "./middlewares/CheckTaskDeletion";
 
-import cors from 'cors'
-import morgan from 'morgan'
-import createError from 'http-errors'
-import dotenv from 'dotenv'
-import express, { NextFunction, Request, Response } from 'express'
-import taskRoutes from './routes/task.routes'
-import { DeleteTaskIfPassesWeekDeleted } from './middlewares/CheckTaskDeletion'
+export const app = express();
 
+app.use(cors());
 
-export const app = express()
+dotenv.config();
 
-DeleteTaskIfPassesWeekDeleted()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cors())
+app.use(morgan("dev"));
 
+app.use("/api", taskRoutes);
 
-dotenv.config()
+DeleteTaskIfPassesWeekDeleted();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError.NotFound());
+});
 
-app.use(morgan('dev'))
-
-
-app.use('/api', taskRoutes)
-
-
-
-app.use((req:Request, res:Response, next: NextFunction) => {
-    next(createError.NotFound())
-})
-
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log(`live at http://localhost:${PORT}`))
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`live at http://localhost:${PORT}`));
--- db/middlewares/task.validate.ts
+++ db/middlewares/task.validate.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 
 const TaskBaseSchema = z.object({
   name: z.string().max(1000, { message: "Task can't be more than 1000 characters" }),
+  description: z.string().max(1000, { message: "Description can't be more than 1000 characters" }).optional(),
+  label: z.array(z.string()).default([]),
   isDone: z.boolean().default(false),
   isDeleted: z.boolean().default(false),
 });
@@ -9,5 +11,5 @@ const TaskBaseSchema = z.object({
 export const TaskCreateValidation = TaskBaseSchema;
 
 export const TaskUpdateValidation = TaskBaseSchema.extend({
-  id: z.string().uuid({ message: "Invalid task id" }),
+  id: z.string().cuid({ message: "Invalid task id" }),
 }).partial();
